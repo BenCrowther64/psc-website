5364f65913f72af06a8f91a01a2280bb9ad1510e
psybc6 <bencrowther64@gmail.com>
2023-03-13 13:26:04 +0000

    People page content complete, paragraph about recent jobs removed but could be placed elsewhere

--- src/pages/People.vue
+++ src/pages/People.vue
@@ -26,26 +26,61 @@ export default {
       <div>
         <div class = directors-container>
           <div class = director>  
-            <img src="../assets/images/people/stock.jpg">
+            <img src="../assets/images/people/paul.jpeg">
               <div class = "text-wrapper">
                 <h3 id = "name">Paul Smith</h3> 
-                <p id = "role">Director of Building Surveying</p> 
-                <p id = "qualifications">RICS Certified Historic Building Professional MRIC</p> 
-                <p>Ian is a Chartered Building Surveyor, Certified Historic Building Professional and Director of Building Surveying at wrdunn. Previously Associate and Senior Building Surveyor, Ian has held a leading role in the repair and restoration of numerous buildings of historic importance and Listed structures together with alterations, extensions and refurbishments to residential, commercial and institutional properties. Ian is one of a small number of professionals nationally to be Accredited in Building Conservation by RICS, and one of only 10 in Yorkshire.</p>
+                <p id = "role">Cost Consultancy and Project Management Director</p> 
+                <p id = "qualifications">MRICS MAPM</p> 
+                <div id ="description"> 
+                  <p>
+                    Paul is a founding director of PSC Surveying Ltd and set up the business in 2011. Paul is an experienced Project Manager and Quantity Surveyor, 
+                    with over 30 years working in the Construction Industry. Paul’s experience includes both new build and refurbishment, from country houses, 
+                    to city centre apartments, retail, commercial, industrial, hospitality and leisure, and sport/leisure, historic and listed buildings.
+                  </p>
+                  <p>
+                    Paul has previously worked for a number of blue chip companies, including International Cost Consultants, International Property Development Companies
+                    and leading leisure and hospitality operators. Paul has a wide ranging knowledge of the development process and specialises in assisting the client development 
+                    manager through the project plan, from feasibility through commercial negotiation through to project delivery and handover.
+                  </p>
+                  <p>
+                    When time allows Paul enjoys spending time with his family, or watching Rugby and Motor sport.
+                  </p>
+                </div>
               </div>
           </div>
           <div class = director>  
-            <img src="../assets/images/people/stock.jpg">  
+            <img src="../assets/images/people/stephen.jpeg">  
             <div class = "text-wrapper">
                 <h3 id = "name">Stephen Crowther</h3> 
-                <p id = "role">Director of Building Surveying</p> 
-                <p id = "qualifications">RICS Certified Historic Building Professional MRIC</p> 
-                <p>Ian is a Chartered Building Surveyor, Certified Historic Building Professional and Director of Building Surveying at wrdunn. Previously Associate and Senior Building Surveyor, Ian has held a leading role in the repair and restoration of numerous buildings of historic importance and Listed structures together with alterations, extensions and refurbishments to residential, commercial and institutional properties. Ian is one of a small number of professionals nationally to be Accredited in Building Conservation by RICS, and one of only 10 in Yorkshire.</p>
-              </div>
+                <p id = "role">Building Surveying and Principal Designer</p> 
+                <p id = "qualifications">MRICS CMaPS Tech IOSH</p> 
+                <div id = "description"> 
+                  <p>
+                    Stephen is a director of PSC Surveying Ltd, a Chartered Building Surveyor and Certified Member of
+                    the Association of Project Safety with over 30 years experience of surveying commercial, industrial
+                    and residential buildings for a range of purposes from acquisition, disposal, defect diagonis and
+                    dilapidations.
+                  </p>
+                  <p>
+                    Stephen also has a wide range of experience in undertaking the role of Principal Designer and Client
+                    (CDM 2015) Adviser under the Construction (Design and Management) Regulations 2015 (CDM
+                    2015) and is accoustomed to working with multidisciplinary design teams to evidence the design risk
+                    management process. 
+                  </p>
+                  <p> 
+                    Before joining PSC Surveying Ltd, Stephen was the managing director of a firm of building surveyors,
+                    principal designers, client (CDM 2015) advisers and asbestos surveyors working throughout the
+                    united kingdom for national clients on large scale new build commecial, industrial and residential
+                    developments as well as refurbishment projects.
+                  </p>
+                  <p> 
+                    Stephen is a keen supporter of Leeds Rhinos, Yorkshire County Cricket Club and Otley Beer Festival.
+                  </p>
+                </div>
             </div>
+          </div>
         </div>
       </div>
-
     </div>
     <Footer></Footer>
 </template>
@@ -100,6 +135,11 @@ export default {
   margin: 0;
 }
 
+#description {
+  margin: 0;
+  max-width: 25vw;
+}
+
 .text-wrapper {
   display: flex;
   flex-direction: column;
@@ -128,13 +168,16 @@ export default {
 }
 
 @media (pointer:none), (pointer:coarse){
+  .directors-wrapper {
+    text-align: center;
+  }
   .directors-container {
     flex-direction: column;
     align-items: center;
   }
   .directors-container img {
     width: 80vw;
-    height: 40vh;
+    height: 50vh;
     object-fit: cover;
   }
   .director {
@@ -160,16 +203,23 @@ export default {
   #name {
     margin-top: 2vh;
     }
+
+  #description{
+    max-width: 80vw;
+  }
   }
 
   @media (max-width: 1080px) {
-    .directors-container {
+  .directors-wrapper {
+    text-align: center;
+  }
+  .directors-container {
     flex-direction: column;
     align-items: center;
   }
   .directors-container img {
     width: 80vw;
-    height: 40vh;
+    height: 50vh;
     object-fit: cover;
   }
   .director {
@@ -195,5 +245,9 @@ export default {
   #name {
     margin-top: 2vh;
     }
+  }
+
+  #description{
+    max-width: 80vw;
   }
 </style>
