f4f922ab16e157bfd652d2e40a397f1f85888fca
psybc6 <bencrowther64@gmail.com>
2023-03-17 17:06:42 +0000

    Added loading screen to application

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<script>
+  import Loading from './components/Loading/Loading.vue'
+
+  export default {
+    data() {
+      return {
+        isLoading: true,
+      }
+    },
+    components: {
+      Loading,
+    },
+    mounted () {
+      setTimeout(() => {
+        this.isLoading = false
+      }, 3000)
+    }
+  }
+</script>
+
+
 <template>
+  <Loading v-if="isLoading" />
+
   <router-view></router-view>
 </template>
---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="loading-screen">
+        <img src="../../assets/images/home/loading-state.gif">
+        <h3>LOADING</h3>
+    </div>  
+</template>
+
+<style scoped>
+    #loading-screen{
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #fff;
+        z-index: 2000;
+    }
+
+    #loading-screen img{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 80px;
+        height: 80px;
+    }
+
+    #loading-screen h3{
+        position: absolute;
+        top: 55%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 1rem;
+        color: var(--psc-dark-blue);
+    }
+
+</style>
